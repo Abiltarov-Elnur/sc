59aa1197e3ea3c696dc17de928dd9845863332ab
Elnur Abiltarov <Asus@DESKTOP-RV00K7V>
2024-11-28 10:33:18 +0300

    sc_4_3

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,28 @@ var router = express.Router();
 
 /* BMW M3 page */
 router.get('/m3', function(req, res, next) {
-  res.send("<h1>BMW M3 page</h1>");
+  res.render("bmw", {
+    title: "BMW M3",
+    picture: "images/bmw_m3.jpg",
+    desc:"lorem20"
+  });
 });
 
 /* BMW M4 page */
 router.get('/m4', function(req, res, next) {
-  res.send("<h1>BMW M4 page</h1>");
+  res.render("bmw", {
+    title: "BMW M4",
+    picture: "images/bmw_m4.jpg",
+    desc:"lorem20"
+  });
 });
 
 /* BMW M5 page */
 router.get('/m5', function(req, res, next) {
-  res.send("<h1>BMW M5 page</h1>");
+  res.render("bmw", {
+    title: "BMW M5",
+    picture: "images/bmw_m5.jpg",
+    desc:"lorem20"
+  });
 });
-
 module.exports = router
