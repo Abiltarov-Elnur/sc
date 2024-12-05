9adb77ee81533cfd83a6a1aedf0080bf6f3adba6
Elnur Abiltarov <Asus@DESKTOP-RV00K7V>
2024-12-05 16:09:36 +0300

    sc_7_2

--- testMongoose.js
+++ testMongoose.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2024');
-const Car = mongoose.model('Car', { name: String });
+var schema = mongoose.Schema({ name: String })
+
+schema.methods.models = function(){
+    console.log(this.name + " M3, M4, M5")
+}
+
+const Car = mongoose.model('Car', schema);
 const bmw = new Car({ name: 'BMW' });
-bmw.save().then(() => console.log('BMW M3'));
+bmw.save().then(() => bmw.models());
+
