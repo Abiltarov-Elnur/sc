20fdbb7ab25ec999fbef7942cc78f1ae28c47161
Elnur Abiltarov <Asus@DESKTOP-RV00K7V>
2025-01-04 23:27:38 +0300

    sc_8_3

--- routes/cars.js
+++ routes/cars.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var Car = require('../models/car').Car;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -12,3 +13,16 @@ router.get("/:nick", function(req, res, next) {
     });
 
 module.exports = router;
+/* Страница котов */
+router.get("/:nick", async function(req, res, next) {
+  var cars = await Car.find({nick: req.params.nick});
+  console.log(cars)
+  if(!cars.length) return next(new Error("Нет такой машины"))
+  var car = cars[0];
+  res.render('car', {
+  title: car.title,
+  picture: car.avatar,
+  desc: car.desc
+  })
+  });
+  
