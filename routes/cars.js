5fd352a5ef3d7effb2c0da9c9815c5c132538c98
Elnur Abiltarov <Asus@DESKTOP-RV00K7V>
2025-01-19 21:09:41 +0300

    sc_10_8

--- routes/cars.js
+++ routes/cars.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var router = express.Router();
 var Car = require('../models/car').Car;
+var checkAuth = require("../middlewares/checkAuth.js");
+
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   res.send('Новый маршрутизатор, для маршрутов, начинающихся с cars');
 });
 
-router.get("/:nick", async function (req, res, next) {
+router.get("/:nick", checkAuth, async function (req, res, next) {
   var cars = await Car.find({ nick: req.params.nick });
   console.log(cars)
   if (!cars.length) return next(new Error("Нет такой машинки"))
